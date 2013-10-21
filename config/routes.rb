5f53dae088c5900217a5187a370d311f459f43e0
Skud <skud@infotrope.net>
2013-10-21 15:37:09 +1100

    rails_apps_composer: controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Acres::Application.routes.draw do
+  get "home/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
