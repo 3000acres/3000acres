831edbcbc59a60781c4c17333db59c2a3ba2a58b
Skud <skud@infotrope.net>
2014-06-04 12:21:14 -0500

    Show posts on site page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :site
   belongs_to :user
 
+  default_scope { order("created_at DESC") }
+
 end
--- spec/features/community_spec.rb
+++ spec/features/community_spec.rb
@@ -14,12 +14,21 @@ feature "post" do
       fill_in 'Address', :with => '1 Smith St'
       fill_in 'Suburb', :with => 'Smithville'
       click_button 'Create Site'
+      @site = Site.last
     end
 
     scenario "post an update about a site" do
-      visit site_path(Site.last)
+      visit site_path(@site)
       page.should have_content "Post an update"
       page.should have_content "Your message"
+      fill_in "Subject", :with => "Test post"
+      fill_in "Your message", :with => "Here is some news about the garden"
+      click_button "Create Post"
+      current_path.should eq site_path(@site)
+      page.should have_content "Test post"
+      page.should have_content "Posted by #{@user.name}"
+      page.should have_content "less than a minute ago"
+      page.should have_content "Here is some news about the garden"
     end
 
   end
