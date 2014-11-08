7dcc09d8984ae8fe41258653a9c7b8709feb9711
Skud <skud@infotrope.net>
2014-11-08 12:29:19 +1100

    Modified tests to take snippetified content into account
    ... since we can't rely on a page necessarily having the content we
    expect any more.

--- spec/features/facebook_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-feature "facebook feed" do
-  scenario "facebook feed shows on front page" do
-    visit root_path
-    page.should have_selector 'div.fb-like-box'
-  end
-end
--- spec/features/footer_spec.rb
+++ spec/features/footer_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 feature "footer" do
   scenario "footer shows on front page" do
     visit root_path
-    page.should have_content "Twitter"
+    expect(page).to have_css "footer"
   end
   scenario "footer shows on other pages" do
     visit new_user_session_path # signin page
-    page.should have_content "Twitter"
+    expect(page).to have_css "footer"
   end
 end
