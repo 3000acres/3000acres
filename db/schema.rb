90fdbf50a2c453f014aeb52b8debfc2963ee6cb9
Rafael Schouten <rafaelschouten@gmail.com>
2015-04-02 13:44:47 +1100

    add migration for site status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328031321) do
+ActiveRecord::Schema.define(version: 20150402014645) do
 
   create_table "comfy_cms_blocks", force: true do |t|
     t.integer  "blockable_id",                    null: false
