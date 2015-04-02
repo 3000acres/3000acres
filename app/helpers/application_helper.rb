145d0ab9fc0ccd0607decfd4a997b23b7e6ab9c3
Rafael Schouten <rafaelschouten@gmail.com>
2015-04-02 11:56:42 +1100

    bugfix and cleaner cms helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,17 +12,14 @@ module ApplicationHelper
     html.html_safe
   end
 
-  def cms_editable_snippet(identifier, cms_site = @cms_site, &block)
+  def cms_editable_snippet(identifier)
 
-    content = cms_snippet_content(identifier, cms_site = @cms_site, &block)
+    content = cms_snippet_content(identifier)
 
-    unless cms_site
-      host, path = request.host_with_port.downcase, request.fullpath if respond_to?(:request) && request
-      cms_site = Comfy::Cms::Site.find_site(host, path)
-    end
-    snippet = cms_site.snippets.find_by_identifier(identifier)
-
-    if user_signed_in? && current_user.has_role?(:admin)
+    host, path = request.host_with_port.downcase, request.fullpath if respond_to?(:request) && request
+    cms_site = Comfy::Cms::Site.find_site(host, path)
+    if !cms_site.nil? && user_signed_in? && current_user.has_role?(:admin)
+      snippet = cms_site.snippets.find_by_identifier(identifier)
       html = <<-HTML
       <div class="snippet-wrapper">
         <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
@@ -38,32 +35,23 @@ module ApplicationHelper
     end
   end
 
-  #   haml = <<-HTML
-# .snippet-wrapper 
-  # %a{href: '#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit', class: 'cms-edit'}
-  #   %span.glyphicon.glyphicon-cog
-  # != "#{content}"
-# HTML
-  #   engine = Haml::Engine.new(haml)
-  #   engine.render
-  # end
-  def cms_snippet_edit_link(identifier, cms_site = @cms_site, &block)
+  def cms_snippet_edit_link(identifier)
+    # links are only for admin
     if !user_signed_in? || !current_user.has_role?(:admin)
       return
     end
 
-    unless cms_site
-      host, path = request.host_with_port.downcase, request.fullpath if respond_to?(:request) && request
-      cms_site = Comfy::Cms::Site.find_site(host, path)
+    host, path = request.host_with_port.downcase, request.fullpath if respond_to?(:request) && request
+    cms_site = Comfy::Cms::Site.find_site(host, path)
+    if !cms_site.nil?
+      snippet = cms_site.snippets.find_by_identifier(identifier)
+
+      html = <<-HTML
+      <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
+        <span class="glyphicon glyphicon-cog"></span>
+      </a>
+      HTML
+      html
     end
-    snippet = cms_site.snippets.find_by_identifier(identifier)
-
-    html = <<-HTML
-    <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
-      <span class="glyphicon glyphicon-cog"></span>
-    </a>
-    HTML
-    html
   end
-
 end
