8836f3a1773e5d77f920297d7d1251e307f6c075
Rafael Schouten <rafaelschouten@gmail.com>
2015-04-02 17:13:24 +1100

    add nil check on snippets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,21 +18,21 @@ module ApplicationHelper
 
     host, path = request.host_with_port.downcase, request.fullpath if respond_to?(:request) && request
     cms_site = Comfy::Cms::Site.find_site(host, path)
+
     if !cms_site.nil? && user_signed_in? && current_user.has_role?(:admin)
       snippet = cms_site.snippets.find_by_identifier(identifier)
-      html = <<-HTML
-      <div class="snippet-wrapper">
-        <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
-          <span class="glyphicon glyphicon-cog"></span>
-        </a>
-        #{content}
-        
-      </div>
-      HTML
-      html
-    else
-      content
+      if !snippet.nil?
+        content = <<-HTML
+        <div class="snippet-wrapper">
+          <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
+            <span class="glyphicon glyphicon-cog"></span>
+          </a>
+          #{content}
+        </div>
+        HTML
+      end
     end
+    content
   end
 
   def cms_snippet_edit_link(identifier)
@@ -45,13 +45,14 @@ module ApplicationHelper
     cms_site = Comfy::Cms::Site.find_site(host, path)
     if !cms_site.nil?
       snippet = cms_site.snippets.find_by_identifier(identifier)
-
-      html = <<-HTML
-      <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
-        <span class="glyphicon glyphicon-cog"></span>
-      </a>
-      HTML
-      html
+      if !snippet.nil?
+        html = <<-HTML
+        <a href='#{request.base_url}/cms-admin/sites/#{cms_site.id}/snippets/#{snippet.id}/edit' class='cms-edit'>
+          <span class="glyphicon glyphicon-cog"></span>
+        </a>
+        HTML
+        html
+      end
     end
   end
 end
