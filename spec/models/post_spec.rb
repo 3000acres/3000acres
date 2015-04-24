1f39b486e4a08711fc4e788b1cdcd22a37843269
Rafael Schouten <rafaelschouten@gmail.com>
2015-04-24 14:03:35 +1000

    test post order

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe Post do
-
   it "has a site and user" do
     @post = FactoryGirl.create(:post)
     @post.site.should be_an_instance_of Site
     @post.user.should be_an_instance_of User
   end
+
+  it "should be ordered ascending, by creation date" do
+    @post1 = FactoryGirl.create(:post, subject: "first")
+    @post2 = FactoryGirl.create(:post, subject: "second")
+    expect(Post.all.to_a).to eq [@post1, @post2]
+  end
+
 end
