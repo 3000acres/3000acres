c12225b292315d46d54f9416206f7a36ba5b33e8
Rafael Schouten <rafaelschouten@gmail.com>
2015-04-24 14:08:03 +1000

    use hash format for default scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :site
   belongs_to :user
 
-  default_scope { order("created_at ASC") }
+  default_scope { order(created_at: :asc) }
 
   after_create :send_post_email
 
