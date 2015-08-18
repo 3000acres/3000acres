344af2f6112d6a949dc08ad55b493fa5b15dbf4a
Rafael Schouten <rafaelschouten@gmail.com>
2015-08-18 13:41:51 +1000

    refactor site spec separating out concerns

---
+++ spec/models/concerns/watches_spec.rb
@@ -0,0 +1,29 @@
+shared_examples "watches" do
+
+  context 'watches' do
+    before(:each) do
+      @site = FactoryGirl.create(:site)
+    end
+
+    it "can watch a site" do
+      expect {
+        FactoryGirl.create(:watch, site: @site)
+      }.to change { @site.watches.count}.by(1)
+    end
+
+    it "auto-watches site when added by a non-admin" do
+      @user = FactoryGirl.create(:user)
+      @this_site = FactoryGirl.create(:site, added_by_user: @user)
+      @this_site.watches.count.should == 1
+      @this_site.watches.last.user.should eq @user
+    end
+
+    it "auto-watches site when added by an admin" do
+      @admin_user = FactoryGirl.create(:admin_user)
+      @new_site = FactoryGirl.create(:site, added_by_user: @admin_user)
+      @new_site.watches.count.should == 1
+      @new_site.watches.last.user.should eq @admin_user
+    end
+  end
+end
+
