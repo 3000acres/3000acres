cb066bd22c3b2f2691cabd6837cf2cf761be6f1c
Rafael Schouten <rafaelschouten@gmail.com>
2015-08-18 23:32:32 +1000

    cleanup image validation and add maximum size

--- app/models/concerns/image.rb
+++ app/models/concerns/image.rb
@@ -2,7 +2,11 @@ module Image
   extend ActiveSupport::Concern
 
   included do 
-    has_attached_file :image, :styles => { :large => "720x720", :medium => "360x360#", :small => "180x180#" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :image, 
+                      :styles => { :large => "720x720", :medium => "360x360#", :small => "180x180#" }, 
+                      :default_url => "/images/:style/missing.png"
+    validates_attachment :image, 
+        :content_type => { :content_type => /\Aimage\/.*\Z/ },
+        :size => { :less_than => 4.megabytes }
   end
 end
