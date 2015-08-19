35a36c23cb7aeea9fd100cb3b4d7d97e04336429
Rafael Schouten <rafaelschouten@gmail.com>
2015-08-19 11:00:49 +1000

    catch graph if errors

--- app/models/concerns/graph.rb
+++ app/models/concerns/graph.rb
@@ -1,20 +1,36 @@
 module Graph
+  # Error handling is mostly for dev not having an id, 
+  # koala has its own error handling for facebook calls.
 
   def self.graph_authorize
-    # @oauth = Koala::Facebook::OAuth.new
-    # @graph = Koala::Facebook::API.new(@oauth.get_app_access_token)
+    begin
+      @oauth = Koala::Facebook::OAuth.new
+      @graph = Koala::Facebook::API.new(@oauth.get_app_access_token)
+    rescue
+      # TODO warning that graph id doesn't authorize
+    end
   end
 
   def self.get_graph_events(id)
-    # graph_authorize
-    # fields = 'id, name, cover, description, end_time' 
-    # @graph.get_connection(id, "events", { fields: fields })
+    begin 
+      graph_authorize
+      fields = 'id, name, cover, description, end_time' 
+      @graph.get_connection(id, "events", { fields: fields })
+    rescue
+      # TODO warning that graph id doesn't authorize
+      []
+    end
   end
 
   def self.get_graph_page(url)
-    # graph_authorize
-    # options = {:params => { id: url }}
-    # @graph.graph_call('', {}, 'get', options) 
+    begin 
+      graph_authorize
+      options = {:params => { id: url }}
+      @graph.graph_call('', {}, 'get', options) 
+    rescue
+      # TODO warning that graph id doesn't authorize
+      Hash.new
+    end
   end
 
 end
